bdb277a3405806c9895c8c0bbbe71a24cde8173d
Philip Thomas <tucono@gmail.com>
2014-02-01 12:06:17 -0800

    Fixed project compilation

--- Console.h
+++ Console.h
@@ -1,5 +1,4 @@
 #pragma once
-#define PATH_MAX 255
 #include <string>
 #include <sys/socket.h> //oh god, vxworks, why
 #include <stdio.h>
--- WPIObjMgr.h
+++ WPIObjMgr.h
@@ -1,5 +1,4 @@
 #pragma once
-#define PATH_MAX 255
 #include <WPILib.h>
 
 namespace Spyder
