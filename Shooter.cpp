de55f24abe01a3c085a8b1e7ba01119c1ba1e741
Philip Thomas <tucono@gmail.com>
2014-02-01 16:53:30 -0800

    Added the autonomous framework for shooter, and added a teleop shooter switch system based on time

--- Shooter.cpp
+++ Shooter.cpp
@@ -2,52 +2,137 @@
 #include "WPIObjMgr.h"
 #include "Config.h"
 #include "Console.h"
+#include <cmath>
 #include <iostream>
 
 class Shooter : public Spyder::Subsystem
 {
 private:
-	/*Spyder::ConfigVar<UINT32> motorShoot1;
+	Spyder::ConfigVar<UINT32> motorShoot1;
 	Spyder::ConfigVar<UINT32> motorShoot2;
-	
+	Spyder::ConfigVar<UINT32> shooterRamp;
 	Spyder::ConfigVar<UINT32> pistonSolenoidExt;
 	Spyder::ConfigVar<UINT32> pistonSolenoidRet;
 	
-	Spyder::TwoIntConfig fireButton;*/
+	Spyder::TwoIntConfig fireButton;
+	Spyder::TwoIntConfig shooterJoystick;
+	
+	bool isPistonOut;
+	float speed;
+	double lastShooterChange;
+	unsigned char firePhase;
+	double fireStart;
 	
 public:
-	Shooter()
+	Shooter() : Spyder::Subsystem("Shooter"), motorShoot1("firstShooterMotor",4), //Get correct numbers
+			motorShoot2("secondShooterMotor", 3), shooterJoystick("adjustShooterHeight", 3, 1),
+			pistonSolenoidExt("shooter_pistonSolenoidExt", 1), pistonSolenoidRet("shooter_pistonSolenoidRet", 2),
+			fireButton("bind_shooterFire", 3, 2), isPistonOut(false), speed(0.f), lastShooterChange(0.0), 
+			shooterRamp("Motor ramp is set", 0.66)
 	{
-		
 	}
 	virtual ~Shooter()
 	{
 	}
 	
-	virtual void Init()
+	virtual void Init(Spyder::RunModes runmode)
 	{
-		
-	}
-	
-	virtual void Perodic(Spyder::RunModes runmode)
-	{
+		Spyder::GetVictor(motorShoot1.GetVal())->Set(0);
+		Spyder::GetVictor(motorShoot2.GetVal())->Set(0);
+		struct timespec tp;
 		switch(runmode)
 		{
+		case Spyder::M_TELEOP:
+			firePhase = 0;
+			clock_gettime(CLOCK_REALTIME, &tp);
+			fireStart = (double)tp.tv_sec + double(double(tp.tv_nsec)*1e-9);
+			break;
+		default:
+			break; 
+		}
+	}
+	
+	virtual void Periodic(Spyder::RunModes runmode)//CHECK IF JOYSTICK IS POS/NEG
+	{
+		
+		switch(runmode)//Winch motor can only drive forwards
+		{
 			case Spyder::M_DISABLED://disabled code here
-				
+			{	Spyder::GetVictor(motorShoot1.GetVal())->Set(0);
+				Spyder::GetVictor(motorShoot2.GetVal())->Set(0);
+				Spyder::GetSolenoid(pistonSolenoidExt.GetVal())->Set(false);
+				Spyder::GetSolenoid(pistonSolenoidRet.GetVal())->Set(true);
+			}
 				break;
 			case Spyder::M_AUTO://autonomous code here
-				
+			{
+				Spyder::GetVictor(motorShoot1.GetVal())->Set(0);
+				Spyder::GetVictor(motorShoot2.GetVal())->Set(0);
+			}
 				break;
-			case Spyder::M_TELEOP://Tele-operation code here
 				
+			case Spyder::M_TELEOP://Tele-operation code here
+			{
+				Joystick *joystick = Spyder::GetJoystick(shooterJoystick.GetVar(1));//get correct joystick number from mechanical
+				float val = joystick->GetRawAxis(shooterJoystick.GetVar(2));
+				val = std::fabs(val) > Spyder::GetDeadzone() ? val : 0;
+				if(val < 0)
+					val=0;
+				
+				struct timespec tp;
+				timespec theTimespec;
+				clock_gettime(CLOCK_REALTIME, &theTimespec);
+				double theTime = theTimespec.tv_sec;
+				theTime+=theTimespec.tv_nsec*1e-9;
+				double curTime = (double)tp.tv_sec + double(double(tp.tv_nsec)*1e-9);
+				double teleopRunTime = curTime - fireStart;
+				
+				//when Kyle presses button, winch down fully
+				if(Spyder::GetJoystick(fireButton.GetVar(1))->GetRawButton(fireButton.GetVar(2)))
+				{
+					Spyder::GetSolenoid(pistonSolenoidExt.GetVal())->Set(true);
+					Spyder::GetSolenoid(pistonSolenoidRet.GetVal())->Set(false);
+					switch(firePhase)//meant to fire then reset
+					{
+						case 0://initialize
+							if (teleopRunTime >= 1)
+							{
+								++firePhase;
+								fireStart = curTime;
+							}
+							break;
+						case 1://Re-engage the arm!
+							Spyder::GetSolenoid(pistonSolenoidExt.GetVal())->Set(false);
+							Spyder::GetSolenoid(pistonSolenoidRet.GetVal())->Set(true);
+							if(teleopRunTime >=0.7)
+							{
+								++firePhase;
+							}
+							break;
+						case 2://Winch it back down !
+							Spyder::GetVictor(motorShoot1.GetVal())->Set(1);
+							Spyder::GetVictor(motorShoot2.GetVal())->Set(1);
+							if(teleopRunTime >1.4)//Get actual time
+							{
+								++firePhase;
+							}
+							break;
+						case 3://Stop motors after winching
+							Spyder::GetVictor(motorShoot1.GetVal())->Set(0);
+							Spyder::GetVictor(motorShoot2.GetVal())->Set(0);
+							break;
+					}
+					
+					/*Spyder::GetVictor(motorShoot1.GetVal())->Set(speed);//find what speed it needs to be
+					Spyder::GetVictor(motorShoot2.GetVal())->Set(speed);*/
+				}
+			}
 				break;
 		};
 	}
 	
 	virtual void RobotInit()
 	{
-		
 	}
 };
 
