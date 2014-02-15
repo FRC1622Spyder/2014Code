da5e9a7423821245747013bc9e9e36db066a81df
Philip Thomas <tucono@gmail.com>
2014-02-15 15:00:06 -0800

    Added curve into pickup:

--- Pickup.cpp
+++ Pickup.cpp
@@ -14,13 +14,14 @@ class Pickup : public Spyder::Subsystem
 		Spyder::ConfigVar<UINT32> arm;
 		Spyder::TwoIntConfig stick;
 		Spyder::ConfigVar<double> rampPickup;
+		Spyder::ConfigVar<float> curvature;
 		double counter;
 		int init;
 public:
 		Pickup() : Spyder::Subsystem("Pickup"), extendSol("pickup_open_sol", 5), releaseSol("pickup_close_sol", 6),
 		eSol("bind_pickup_close", 3, 6), rSol("bind_pickup_open", 3, 7), 
 		arm("pickup_arm_mot", 12), stick("bind_pickup_arm_axis", 3, 2), 
-		rampPickup("pickup_ramp", 0.666), counter(0.), init(0)
+		rampPickup("pickup_ramp", 0.666), counter(0.), init(0), curvature("drive_curvature",0.3f)
 		{
 			
 		}	 
@@ -34,26 +35,28 @@ public:
 			Spyder::GetSolenoid(extendSol.GetVal())->Set(false);
 			Spyder::GetSolenoid(releaseSol.GetVal())->Set(true);
 			Spyder::GetVictor(arm.GetVal())->Set(0.0f);
-
 		}
 
 		virtual void Periodic(Spyder::RunModes runmode)
 		{
-				switch(runmode)
+			float motor = 0.0f;
+			float curve = curvature.GetVal();
+			Joystick *sticky  = Spyder::GetJoystick(stick.GetVar(1));
+			
+			switch(runmode)
 				{
 				case Spyder::M_DISABLED:
 					Spyder::GetSolenoid(extendSol.GetVal())->Set(false);
 					Spyder::GetSolenoid(releaseSol.GetVal())->Set(true);
 					Spyder::GetVictor(arm.GetVal())->Set(0.0f);	
-						break;
+					break;
 				case Spyder::M_TELEOP:
-					Spyder::GetVictor(arm.GetVal())->Set(Spyder::GetJoystick(stick.GetVar(1))->GetRawAxis(stick.GetVar(2)));
-					if(init < 10);
-					{
-						 counter = rampPickup.GetVal() * Spyder::GetJoystick(stick.GetVar(1))->GetRawAxis(stick.GetVar(1));
-						 Spyder::GetVictor(arm.GetVal())->Set(counter);
-						 init++;
-					}
+					Spyder::GetVictor(arm.GetVal())->Set(sticky->GetRawAxis(stick.GetVar(2)));
+					motor = sticky->GetRawAxis(stick.GetVar(1));
+					
+					motor = ((motor*motor*motor)*curve + (motor*(1.f-curve)));
+				
+					
 					
 					if(Spyder::GetJoystick(eSol.GetVar(1))->GetRawButton(eSol.GetVar(2))==true) {
 						Spyder::GetSolenoid(extendSol.GetVal())->Set(true);
