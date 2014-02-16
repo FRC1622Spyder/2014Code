7a417beb2b4b3433420f9b126d8a44ad34b42d3c
Michael Salamy <michaelsalamy@gmail.com>
2014-02-16 13:35:08 -0800

    Added autonomous framework; drive

--- Drive.cpp
+++ Drive.cpp
@@ -3,6 +3,7 @@
 #include "Config.h"
 #include <math.h>
 #include <iostream>
+#include <cmath>
 
 class Drive : public Spyder::Subsystem
 {
@@ -17,14 +18,17 @@ class Drive : public Spyder::Subsystem
 		Spyder::ConfigVar<bool> leftMotorInv;
 		Spyder::ConfigVar<bool> rightMotorInv;
 		
+		Spyder::TwoIntConfig halfSpeed;
+		Spyder::ConfigVar<double> ramp;
+		
 		bool reversed;
 		bool lastRevBtnVal;
 		Spyder::TwoIntConfig reverseBtn;
-		Spyder::TwoIntConfig halfSpeed;
 		
-		Spyder::ConfigVar<double> ramp;
-		
-		Spyder::ConfigVar<float> curvature;
+		float autospeed;
+		float invautospeed;
+		float autotime;
+		double driveStart;
 	
 	public:
 		
@@ -33,7 +37,8 @@ class Drive : public Spyder::Subsystem
 				rightMotor("rightDriveMotor", 1), leftMotorInv("leftDriveInverted", true),
 				rightMotorInv("rightDriveInverted", false), reversed(false),
 				lastRevBtnVal(false), reverseBtn("bind_driveReverse", 2, 3),
-				halfSpeed("bind_halfSpeedDrive", 1, 1), ramp("drive_ramp", 0.666), curvature("drive_curvature",0.3f)
+				halfSpeed("bind_halfSpeedDrive", 1, 1), ramp("drive_ramp", 0.666), autospeed(127.f),
+				invautospeed(-127.f), driveStart(0), autotime(10000.f)
 		{
 		}
 		
@@ -49,11 +54,32 @@ class Drive : public Spyder::Subsystem
 		{
 			float right = 0.0f;
 			float left = 0.0f;
-			float curve = curvature.GetVal();
 			Joystick *leftJoy = Spyder::GetJoystick(leftJoystick.GetVar(1));
 			Joystick *rightJoy = Spyder::GetJoystick(rightJoystick.GetVar(1));
 			
 			switch(runmode){
+				case Spyder::M_AUTO:
+					struct timespec tp;
+					timespec theTimespec;
+					clock_gettime(CLOCK_REALTIME, &theTimespec);
+					double theTime = theTimespec.tv_sec;
+					theTime+=theTimespec.tv_nsec*1e-9;
+					double curTime = (double)tp.tv_sec + double(double(tp.tv_nsec)*1e-9);
+					double teleopRunTime = curTime - driveStart;
+					
+					if(teleopRunTime < autotime)
+					{
+						Spyder::GetVictor(leftMotor.GetVal())->Set(invautospeed);
+						Spyder::GetVictor(rightMotor.GetVal())->Set(autospeed);
+					}
+					else
+					{
+						Spyder::GetVictor(leftMotor.GetVal())->Set(0);
+						Spyder::GetVictor(rightMotor.GetVal())->Set(0);
+					}
+					
+					break;
+				
 				case Spyder::M_DISABLED:
 					Spyder::GetVictor(leftMotor.GetVal())->Set(0);
 					Spyder::GetVictor(leftMotor.GetVal())->Set(0);
@@ -64,9 +90,6 @@ class Drive : public Spyder::Subsystem
 					right = rightJoy->GetRawAxis(rightJoystick.GetVar(2));
 					left = fabs(left) > Spyder::GetDeadzone() ? left : 0;
 					right = fabs(right) > Spyder::GetDeadzone() ? right : 0;
-					
-					left = ((left*left*left)*curve + (left*(1.f-curve)));
-					right = ((right*right*right)*curve + (right*(1.f-curve)));
 					
 					if(leftMotorInv.GetVal())
 					{
