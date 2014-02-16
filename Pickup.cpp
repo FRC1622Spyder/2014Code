4c47959bbcff0b8a103c4ca5313077fa6ee3c717
Philip Thomas <tucono@gmail.com>
2014-02-15 16:37:40 -0800

    Attempted motor-button fix; pickup

--- Pickup.cpp
+++ Pickup.cpp
@@ -19,7 +19,7 @@ class Pickup : public Spyder::Subsystem
 public:
 		Pickup() : Spyder::Subsystem("Pickup"), extendSol("pickup_open_sol", 5), releaseSol("pickup_close_sol", 6),
 		eSol("bind_pickup_close", 3, 6), rSol("bind_pickup_open", 3, 7), 
-		arm("pickup_arm_mot", 12), pickButton_in("bind_intake_button", 3, 2), pickButton_out("bind_outake_button",4,2)
+		arm("pickup_arm_mot", 12), pickButton_in("bind_intake_button", 3, 2), pickButton_out("bind_outake_button",4,3)
 		{	
 		}	 
 		virtual ~Pickup() 
@@ -27,6 +27,7 @@ public:
 		}	
 	
 		virtual void Init(Spyder::RunModes runmode)
+		
 		{
 			Spyder::GetSolenoid(extendSol.GetVal())->Set(false);
 			Spyder::GetSolenoid(releaseSol.GetVal())->Set(true);
@@ -43,7 +44,30 @@ public:
 					Spyder::GetVictor(arm.GetVal())->Set(0);	
 					break;
 				case Spyder::M_TELEOP:
+					int pickSwitch;
 					if(Spyder::GetJoystick(pickButton_in.GetVar(1))->GetRawButton(pickButton_in.GetVar(2)))
+					{
+						pickSwitch = 1;
+					}
+					if(Spyder::GetJoystick(pickButton_out.GetVar(1))->GetRawButton(pickButton_out.GetVar(2)))
+					{
+						pickSwitch = 2;
+					}
+					
+					switch(pickSwitch)
+					{
+					case 1:
+						Spyder::GetVictor(arm.GetVal())->Set(1);
+						break;
+					case 2:
+						Spyder::GetVictor(arm.GetVal())->Set(-1);
+						break;
+					default:
+						Spyder::GetVictor(arm.GetVal())->Set(0);
+						break;
+					}
+					
+					/*if(Spyder::GetJoystick(pickButton_in.GetVar(1))->GetRawButton(pickButton_in.GetVar(2)))
 					{
 						Spyder::GetVictor(arm.GetVal())->Set(1);
 					}
@@ -58,7 +82,7 @@ public:
 					else
 					{
 						Spyder::GetVictor(arm.GetVal())->Set(0);
-					}
+					}*/
 					
 					if(Spyder::GetJoystick(eSol.GetVar(1))->GetRawButton(eSol.GetVar(2))==true) {
 						Spyder::GetSolenoid(extendSol.GetVal())->Set(true);
