0334992e80f200ba0b963d66ab93085c185a9571
Philip Thomas <tucono@gmail.com>
2014-02-16 13:49:43 -0800

    Removed redundant code; shooter

--- Shooter.cpp
+++ Shooter.cpp
@@ -14,31 +14,30 @@ private:
 	Spyder::ConfigVar<UINT32> pistonSolenoidRet;
 	Spyder::ConfigVar<double> firePhase1Time;
 	Spyder::ConfigVar<double> firePhase2Time;
-	Spyder::ConfigVar<double> firePreset1;
-	Spyder::ConfigVar<double> firePreset2;
-	Spyder::ConfigVar<double> firePreset3;
+	Spyder::ConfigVar<float> firePreset1;
+	Spyder::ConfigVar<float> firePreset2;
+	Spyder::ConfigVar<float> firePreset3;
 
 	Spyder::TwoIntConfig fireButton;
 	Spyder::TwoIntConfig fireWinch1;//winch preset location1
 	Spyder::TwoIntConfig fireWinch2;
 	Spyder::TwoIntConfig fireWinch3;
 	
-	bool isPistonOut;
-	float speed;
-	double lastShooterChange;
 	unsigned char firePhase;
 	double fireStart;
+	float autofireStart;
+	unsigned char autofirePhase;
 	double winchTime;
 	
 public:
 	Shooter() : Spyder::Subsystem("Shooter"), motorShoot1("firstShooterMotor",4), //Get correct numbers
 			motorShoot2("secondShooterMotor", 3), pistonSolenoidExt("shooter_pistonSolenoidExt", 1), 
 			pistonSolenoidRet("shooter_pistonSolenoidRet", 2), firePhase1Time ("shooter_firetime1", 1),
-			firePhase2Time("shooter_firetime2", 1), firePreset1("winch_preset1", 1),
-			firePreset2("winch_preset2", 0.75),firePreset3("winch_preset3", 0.5),
+			firePhase2Time("shooter_firetime2", 1), firePreset1("winch_time_preset1", 1),
+			firePreset2("winch_time_preset2", 0.75),firePreset3("winch_time_preset3", 0.5),
 			fireButton("bind_shooterFire1", 3, 2), fireWinch1("winch_pos1", 3, 1), 
-			fireWinch2("winch_pos2", 3, 3), fireWinch3("winch_pos3", 3, 4), isPistonOut(false), 
-			speed(0.f), lastShooterChange(0.0), firePhase(0), fireStart(0), winchTime(0)
+			fireWinch2("winch_pos2", 3, 3), fireWinch3("winch_pos3", 3, 4),
+			firePhase(0), fireStart(0), winchTime(0)
 	{
 	}
 	virtual ~Shooter()
@@ -52,6 +51,11 @@ public:
 		struct timespec tp;
 		switch(runmode)
 		{
+		case Spyder::M_AUTO:
+			firePhase = 0;
+			clock_gettime(CLOCK_REALTIME, &tp);
+			autofireStart = (double)tp.tv_sec + double(double(tp.tv_nsec)*1e-9);
+			break;
 		case Spyder::M_TELEOP:
 			firePhase = 0;
 			clock_gettime(CLOCK_REALTIME, &tp);
@@ -76,21 +80,54 @@ public:
 				break;
 			case Spyder::M_AUTO://autonomous code here
 			{
-				Spyder::GetVictor(motorShoot1.GetVal())->Set(0);
-				Spyder::GetVictor(motorShoot2.GetVal())->Set(0);
+				winchTime = firePreset3.GetVal();
+				struct timespec tp;
+				timespec theTimespec;
+				clock_gettime(CLOCK_REALTIME, &theTimespec);
+				double theTime = theTimespec.tv_sec;
+				theTime+=theTimespec.tv_nsec*1e-9;
+				double curTime = (double)tp.tv_sec + double(double(tp.tv_nsec)*1e-9);
+				double autoRunTime = curTime - autofireStart;
+				switch(autofirePhase)
+				{
+				case 1:
+					Spyder::GetSolenoid(pistonSolenoidRet.GetVal())->Set(false);
+					Spyder::GetSolenoid(pistonSolenoidExt.GetVal())->Set(true);
+					if(autoRunTime > firePhase1Time.GetVal())
+					{
+						autofireStart = curTime;
+						autofirePhase++;
+					}
+					break;
+				case 2:
+					Spyder::GetSolenoid(pistonSolenoidExt.GetVal())->Set(false);
+					Spyder::GetSolenoid(pistonSolenoidRet.GetVal())->Set(true);
+					break;
+				case 0:
+					Spyder::GetVictor(motorShoot1.GetVal())->Set(1);
+					Spyder::GetVictor(motorShoot2.GetVal())->Set(1);
+					if(autoRunTime > winchTime)
+					{
+						Spyder::GetVictor(motorShoot1.GetVal())->Set(0);
+						Spyder::GetVictor(motorShoot2.GetVal())->Set(0);
+						autofireStart = curTime;
+						autofirePhase++;
+					}
+					break;
+				}
 			}
 				break;
 			case Spyder::M_TEST:
 			case Spyder::M_TELEOP://Tele-operation code here
 			{	
 				if(Spyder::GetJoystick(fireWinch1.GetVar(1))->GetRawButton(fireWinch1.GetVar(2)))
-					winchTime = 1;//Trial + Error the shit out of this
+					winchTime = firePreset1.GetVal();//Trial + Error the shit out of this
 				
 				if(Spyder::GetJoystick(fireWinch1.GetVar(1))->GetRawButton(fireWinch1.GetVar(2)))
-					winchTime = 2;//Ditto
+					winchTime = firePreset2.GetVal();//Ditto
 				
 				if(Spyder::GetJoystick(fireWinch1.GetVar(1))->GetRawButton(fireWinch1.GetVar(2)))
-					winchTime = 3;//Ditto
+					winchTime = firePreset3.GetVal();//Ditto
 				
 				struct timespec tp;
 				timespec theTimespec;
