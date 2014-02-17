369f3c8d655f501965fadbe824f8e3a9a0db5275
Philip Thomas <tucono@gmail.com>
2014-02-17 15:26:02 -0800

    removed redundant code; pickup

--- Pickup.cpp
+++ Pickup.cpp
@@ -15,7 +15,6 @@ class Pickup : public Spyder::Subsystem
 		Spyder::ConfigVar<UINT32> arm;
 		Spyder::TwoIntConfig pickButton_in;
 		Spyder::TwoIntConfig pickButton_out;
-
 public:
 		Pickup() : Spyder::Subsystem("Pickup"), extendSol("pickup_open_sol", 5), releaseSol("pickup_close_sol", 6),
 		eSol("bind_pickup_close", 3, 5), rSol("bind_pickup_open", 3, 6), 
@@ -43,6 +42,9 @@ public:
 					Spyder::GetSolenoid(releaseSol.GetVal())->Set(true);
 					Spyder::GetVictor(arm.GetVal())->Set(0);	
 					break;
+				case Spyder::M_AUTO:
+					Spyder::GetSolenoid(extendSol.GetVal())->Set(true);
+					Spyder::GetSolenoid(releaseSol.GetVal())->Set(false);
 				case Spyder::M_TELEOP:
 					int pickSwitch;
 					if(Spyder::GetJoystick(pickButton_in.GetVar(1))->GetRawButton(pickButton_in.GetVar(2)))
@@ -67,23 +69,6 @@ public:
 						break;
 					}
 					
-					/*if(Spyder::GetJoystick(pickButton_in.GetVar(1))->GetRawButton(pickButton_in.GetVar(2)))
-					{
-						Spyder::GetVictor(arm.GetVal())->Set(1);
-					}
-					else
-					{
-						Spyder::GetVictor(arm.GetVal())->Set(0);
-					}
-					if(Spyder::GetJoystick(pickButton_out.GetVar(1))->GetRawButton(pickButton_out.GetVar(2)))
-					{
-						Spyder::GetVictor(arm.GetVal())->Set(-1);
-					}
-					else
-					{
-						Spyder::GetVictor(arm.GetVal())->Set(0);
-					}*/
-					
 					if(Spyder::GetJoystick(eSol.GetVar(1))->GetRawButton(eSol.GetVar(2))==true) {
 						Spyder::GetSolenoid(extendSol.GetVal())->Set(true);
 						Spyder::GetSolenoid(releaseSol.GetVal())->Set(false);
@@ -104,7 +89,5 @@ public:
 		virtual void RobotInit()
 		{
 		}
-		
 };
-
 Pickup pickup;
