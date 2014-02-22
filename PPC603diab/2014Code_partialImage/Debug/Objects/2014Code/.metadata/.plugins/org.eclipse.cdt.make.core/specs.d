f3ce20896b7462c7dd4603a13caf5f3f29c71bb3
Philip Thomas <tucono@gmail.com>
2014-02-22 12:01:22 -0800

    working on fixing the build

---
+++ PPC603diab/2014Code_partialImage/Debug/Objects/2014Code/.metadata/.plugins/org.eclipse.cdt.make.core/specs.d
@@ -0,0 +1,9 @@
+2014Code_partialImage/Debug/Objects/2014Code/.metadata/.plugins/org.eclipse.cdt.make.core/specs.o: \
+  Z:/workspace/2014Code/.metadata/.plugins/org.eclipse.cdt.make.core/specs.cpp \
+  C:\WindRiver\diab\5.4.0.0\include\diab\lpragma.h \
+  C:\WindRiver\diab\5.4.0.0\include\diab\xmacros.h
+
+C:\WindRiver\diab\5.4.0.0\include\diab\lpragma.h:
+
+C:\WindRiver\diab\5.4.0.0\include\diab\xmacros.h:
+
