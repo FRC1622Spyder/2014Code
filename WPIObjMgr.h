48044528958fb36abb70b456f07b14b99bf6d468
Ian Crawford <ian.crawford.frc.elec.1@gmial.com>
2014-03-22 16:25:31 -0700

    Since maps and hash maps have a ton of overhead (256 b minumim for this system) this is an array based implimentation of WPIObjMgr. Main code will likely need to be reworked.

--- WPIObjMgr.h
+++ WPIObjMgr.h
@@ -3,10 +3,30 @@
 
 namespace Spyder
 {
-	Joystick *GetJoystick(UINT32 port);
-	Relay *GetRelay(UINT32 channel);
-	Victor* GetVictor(UINT32 channel);
-	Solenoid* GetSolenoid(UINT32 channel);
-	Encoder* GetEncoder(UINT32 aChannel, UINT32 bChannel, bool inverse);
-	double GetDeadzone();
+	double GetDeadzone(); //shouldn't this go in a utilities file?
+	struct IOCfg{
+		Relay::Direction relayModes[8];
+		uint8_t numEncoders;
+		uint8_t aChanels[5];
+		uint8_t bChanels[5];
+		bool encoderReverse[5];
+	};
+	class WPIObjMgr {
+		Joystick *m_joysticks[4];
+		Victor *m_victors[10];
+		Solenoid * m_solenoids[8];
+		Encoder * m_encoders[5];
+		Relay * m_relays[8];
+		void baseInit();
+	public:
+		WPIObjMgr();
+		WPIObjMgr(IOCfg config);
+		bool addEncoder(UINT32 aChan, UINT32 bChan, bool reverse);
+		Joystick *GetJoystick(uint8_t port);
+		Relay *GetRelay(uint8_t channel);
+		Victor* GetVictor(uint8_t channel);
+		Solenoid* GetSolenoid(uint8_t channel);
+		Encoder* GetEncoder(uint8_t index);
+		
+	};
 };
